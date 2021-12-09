cad8fdb18909953ca46a2ffa4cbb6c9e98881b8e
Carmel <heydarianc@gmail.com>
2021-12-08 20:38:20 -0700

    2nd Commit
    Resolved an error

--- src/WriteNote.js
+++ src/WriteNote.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Row,Col,Form} from 'react-bootstrap';
-import './WriteNoteStyle.css'
+
 
 
 function WriteNote() {
